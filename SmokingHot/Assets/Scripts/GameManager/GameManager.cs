0129f1cc02c9e16ebcd220f01fa5f860e60154ea
Simon Walther <simon.walt@hotmail.com>
2024-10-30 20:51:52 +0100

    Add a scene with table

--- SmokingHot/Assets/Scripts/GameManager/GameManager.cs
+++ SmokingHot/Assets/Scripts/GameManager/GameManager.cs
@@ -3,19 +3,4 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameUI gameUI;
-    public uint money = 1000;
-    private uint cigaretteShopCost = 100;
-    
-    public void buyCigaretteShop()
-    {
-        if (money >= cigaretteShopCost)
-        {
-            money -= cigaretteShopCost;
-            gameUI.updateMoneyText();
-        }
-        else
-        {
-            Debug.Log($"{money} not enough to buy shop ({cigaretteShopCost})");
-        }
-    }
 }
--- SmokingHot/Assets/Scripts/UI/GameUI.cs
+++ SmokingHot/Assets/Scripts/UI/GameUI.cs
@@ -4,22 +4,9 @@ using TMPro;
 public class GameUI : MonoBehaviour
 {
     public TMP_Text companyName;
-    public TMP_Text moneyText;
-    public GameManager gameManager;
 
     void Start()
     {
         companyName.text = MainManager.Instance.companyName;
-        updateMoneyText();
     }
-
-    void Update()
-    {
-
-    }
-
-    public void updateMoneyText()
-    {
-        moneyText.text = $"${gameManager.money}";
-    }    
 }
